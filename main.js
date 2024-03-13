243ee841b71aa9b7cb93d30a4ccdc53f946b3283
Алексей Поплаухин <infopop@mail.ru>
2024-03-13 12:48:21 +0300

    Написал карусель

---
+++ main.js
@@ -0,0 +1,49 @@
+const carousel = document.querySelector('.carousel');
+const prevButton = document.getElementById('prevBtn');
+const nextButton = document.getElementById('nextBtn');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].offsetWidth * 3; // Ширина трех слайдов
+const totalSlides = slides.length;
+let currentIndex = 0;
+
+const counter = document.getElementById('counter');
+
+updateCounter();
+updateButtons();
+
+nextButton.addEventListener('click', () => {
+  currentIndex = Math.min(currentIndex + 3, totalSlides - 3);
+  updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+  currentIndex = Math.max(currentIndex - 3, 0);
+  updateCarousel();
+});
+
+function updateCarousel() {
+  const offset = -currentIndex * slideWidth / 3;
+  carousel.style.transform = `translateX(${offset}px)`;
+  updateCounter();
+  updateButtons();
+};
+
+function updateButtons() {
+  if (currentIndex === 0) {
+    prevButton.classList.add('disabled');
+  } else {
+    prevButton.classList.remove('disabled');
+  };
+
+  if (currentIndex >= totalSlides - 3) {
+    nextButton.classList.add('disabled');
+  } else {
+    nextButton.classList.remove('disabled');
+  };
+};
+
+function updateCounter() {
+  const startSlide = currentIndex === 0 ? 1 : currentIndex + 3;
+  const endSlide = Math.min(currentIndex + 3, totalSlides);
+  counter.textContent = `${startSlide}/${totalSlides}`;
+};
