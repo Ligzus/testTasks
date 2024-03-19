3b74a554f53c0901716b69f7ce5d849b26a2f7cd
Ligzus <153594049+Ligzus@users.noreply.github.com>
2024-03-19 22:57:51 +0300

    Все готово

--- main.js
+++ main.js
@@ -47,3 +47,43 @@ function updateCounter() {
   const endSlide = Math.min(currentIndex + 3, totalSlides);
   counter.textContent = `${startSlide}/${totalSlides}`;
 };
+
+// МОБИЛЬНЫЕ КАРУСЕЛИ:
+
+let ourCurrentSlide = 0; // Используем индексацию с 0
+const ourTotalSlides = document.querySelectorAll('.stages__mobile').length; // Получаем общее количество слайдов
+
+function showOurSlide(n) {
+    const slides = document.querySelectorAll('.stages__mobile'); // Получаем все слайды
+    slides.forEach(slide => slide.classList.remove('active')); // Удаляем класс 'active' у всех слайдов
+    slides[n].classList.add('active'); // Добавляем класс 'active' текущему слайду
+    ourCurrentSlide = n; // Обновляем текущий слайд
+    updateOurButtons(); // Обновляем состояние кнопок
+}
+
+function nextOurSlide() {
+    if (ourCurrentSlide < ourTotalSlides - 1) { // Проверяем, есть ли следующий слайд
+        showOurSlide(ourCurrentSlide + 1); // Показываем следующий слайд
+    }
+}
+
+function prevOurSlide() {
+    if (ourCurrentSlide > 0) { // Проверяем, есть ли предыдущий слайд
+        showOurSlide(ourCurrentSlide - 1); // Показываем предыдущий слайд
+    }
+}
+
+function updateOurButtons() {
+    const prevBtn = document.getElementById('prevBtnStage');
+    const nextBtn = document.getElementById('nextBtnStage');
+    prevBtn.disabled = ourCurrentSlide === 0; // Отключаем кнопку "предыдущий", если текущий слайд первый
+    nextBtn.disabled = ourCurrentSlide === ourTotalSlides - 1; // Отключаем кнопку "следующий", если текущий слайд последний
+}
+
+showOurSlide(ourCurrentSlide); // Показываем начальный слайд
+
+document.getElementById('prevBtnStage').addEventListener('click', prevOurSlide); // Добавляем обработчик события для кнопки "предыдущий"
+document.getElementById('nextBtnStage').addEventListener('click', nextOurSlide); // Добавляем обработчик события для кнопки "следующий"
+
+// МОБИЛЬНАЯ КАРУСЕЛЬ2:
+
